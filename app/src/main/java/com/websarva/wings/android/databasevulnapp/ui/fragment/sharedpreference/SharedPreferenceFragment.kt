394e5e5e323882223a6758a8a0171eadc51829cb
daiki0508 <oochani05085@outlook.jp>
2021-11-22 17:39:24 +0900

    feat: Added sharedPreferences problem layout.

---
+++ app/src/main/java/com/websarva/wings/android/databasevulnapp/ui/fragment/sharedpreference/SharedPreferenceFragment.kt
@@ -0,0 +1,21 @@
+package com.websarva.wings.android.databasevulnapp.ui.fragment.sharedpreference
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SharedPreferenceFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
