acdda5f00ca5cebcbbf56b3ce448dfd4a8b4cee9
daiki0508 <oochani05085@outlook.jp>
2021-11-26 11:15:00 +0900

    fix: Fixed because the process when tapping the back button on the Database problem screen was not implemented.

--- app/src/main/java/com/websarva/wings/android/databasevulnapp/ui/fragment/database/DatabaseFragment.kt
+++ app/src/main/java/com/websarva/wings/android/databasevulnapp/ui/fragment/database/DatabaseFragment.kt
@@ -1,13 +1,17 @@
 package com.websarva.wings.android.databasevulnapp.ui.fragment.database
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.websarva.wings.android.databasevulnapp.R
 import com.websarva.wings.android.databasevulnapp.databinding.FragmentDatabaseBinding
+import com.websarva.wings.android.databasevulnapp.ui.MainActivity
 import com.websarva.wings.android.databasevulnapp.ui.fragment.sharedpreference.AlertDialogFragment
 import com.websarva.wings.android.databasevulnapp.viewmodel.database.DatabaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +36,17 @@ class DatabaseFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // backボタンタップ時の処理
+        requireActivity().onBackPressedDispatcher.addCallback(this){
+            activity?.let {
+                with(Intent(it, MainActivity::class.java)){
+                    it.finish()
+                    startActivity(this)
+                    it.overridePendingTransition(R.anim.fragment_pop_enter, R.anim.fragment_pop_exit)
+                }
+            }
+        }
 
         // databaseの作成
         viewModel.createDatabase()
